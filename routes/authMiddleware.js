b58760c363fce946ae4b435f86308b381bd55813
replakcan <alper.makcan@gmail.com>
2025-07-09 19:16:31 +0300

    Creates mw to check if a user is logged in

---
+++ routes/authMiddleware.js
@@ -0,0 +1,11 @@
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.status(401).json({ msg: 'You are not authorized to view this resource' })
+  }
+}
+
+module.exports = {
+  isAuth,
+}
